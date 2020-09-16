523a4f653811ef1465fccd4b6105e813e61fbfe3
rainavyas <sayvaniar@gmail.com>
2020-09-16 22:48:24 +0100

    added model class

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
 import pytorch
+
+
+class Basic_two_layer(torch.nn.Module):
+    def __init__(self, image_size, num_digits):
+
+        super(Basic_single_layer, self).__init__()
+        self.layer1 = torch.nn.Linear(2*num_digits, image_size, bias = True)
+        self.layer1 = torch.nn.Linear(num_digits, 2*num_digits, bias = True)
+
+    def forward(self, X):
+        out1= self.layer1(X)
+        out1_sig = torch.sigmoid(out1)
+        y_unnormal = self.layer(out1_sig)
+        y_SF = torch.nn.Softmax(y_unnormal, dim=1)
+        #y_out = torch.argmax(y_SF, dim=1)
+        return y_SF
--- train.py
+++ train.py
@@ -1,5 +1,14 @@
 import torch
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
 import csv
+from models import Basic_two_layer
+
+IMG_DIM = 28
+
+# Set seed for reproducibility
+seed = 1
+torch.manual_seed(seed)
 
 # Read in Data
 y = []
@@ -20,13 +29,8 @@ with open('train.csv', newline='') as csvfile:
         X.append(row_vals[1:])
         y.append(row_vals[0])
 
-
-print(len(X))
-print(len(y))
-
-
 # Separate into Training and Dev sets (80:20 ratio)
-training_size = 0.8*len(X)
+training_size = int(0.8*len(X))
 X_train = X[:training_size]
 X_dev = X[training_size:]
 y_train = y[:training_size]
@@ -37,3 +41,29 @@ X_train = torch.FloatTensor(X_train)
 X_dev = torch.FloatTensor(X_dev)
 y_train = torch.FloatTensor(y_train)
 y_dev = torch.FloatTensor(y_dev)
+
+# Reshape input tensors into images
+X_train = torch.reshape(X_train, (-1, IMG_DIM, IMG_DIM))
+X_dev = torch.reshape(X_train, (-1, IMG_DIM, IMG_DIM))
+
+
+# Mini-batch size
+bs = 100
+epochs = 8
+lr = 1e-1
+
+# Store all training dataset in a single wrapped tensor
+train_ds = TensorDataset(X, y_train)
+
+# Use DataLoader to handle minibatches easily
+train_dl = DataLoader(train_ds, batch_size = bs, shuffle = True)
+
+# Construct model
+my_model = Basic_two_layer(IMG_DIM, 10)
+my_model = my_model.float()
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(my_model.parameters(), lr=lr)
+
+
+# Pass through model 
