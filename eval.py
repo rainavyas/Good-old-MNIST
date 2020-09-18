dd2f6e93c35ffe0dfa3ad121227e08ec0371877c
rainavyas <sayvaniar@gmail.com>
2020-09-18 01:49:17 +0100

    file link

--- eval.py
+++ eval.py
@@ -1,6 +1,6 @@
 import torch
 import csv
-from models import Simple_DNN, Basic_two_layer
+from models import Simple_DNN, Basic_two_layer, Simple_CNN
 
 IMG_DIM = 28
 
@@ -27,7 +27,7 @@ X = torch.FloatTensor(X)
 X = torch.reshape(X, (-1, IMG_DIM, IMG_DIM))
 
 # Load model
-model_path = "Simple_DNN.pt"
+model_path = "Simple_CNN.pt"
 model = torch.load(model_path)
 model.eval()
 
@@ -36,7 +36,7 @@ y = torch.argmax(y_probs, dim = 1)
 y = y.tolist()
 
 # Write to csv file
-file_name = "Simple_DNN_test_output.csv"
+file_name = "Simple_CNN_test_output.csv"
 with open(file_name, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
